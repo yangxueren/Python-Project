ecd887fa77358ee74f46f6338f562d748ae57395
yangxueren <yangxueren2017@gmail.com>
2021-11-11 19:00:40 +0800

    patch 2021/11/11 实现了将提气数据通过pymatplotlib绘制成图表并保存为本地的图片文件，然后将图片文件转换成Base64和MD5值并推送到企业微信群中。

--- WehookNotification/wechatRobot.py
+++ WehookNotification/wechatRobot.py
@@ -7,6 +7,7 @@ from matplotlib import pyplot as plt
 from matplotlib.pyplot import MultipleLocator
 import numpy as np
 import time
+import datetime
 import base64
 import hashlib
 
@@ -58,25 +59,36 @@ def querySql():
 
     connection = conn()
     cursor = connection.cursor()
-    content = []
+    content1 = []
+    content2 = []
     for i in range(int(hour)):
         sql = """select SUM(isnull(Energy,0))/54.55 from gmspdb04.dbo.S2_HourlyStationBalance where + StnId in (select Id from gmspdb04.dbo.S2_Stn where ObjTypeId=89 and Name <> 'HKCGB1' and Name <> 'TRN1' and Name <> 'SZLNG') 
-            AND TIME='%s %s:00:00' """ % (day, i)
+            AND TIME='%s %s:00:00' """ % (today, i)
         cursor.execute(sql)
         row = cursor.fetchone()
         row_value = row[0]
-        content.append(row_value)
+        content1.append(row_value)
+
+    for i in range(int(hour)):
+        sql = """select SUM(isnull(Energy,0))/54.55 from gmspdb04.dbo.S2_HourlyStationBalance where + StnId in (select Id from gmspdb04.dbo.S2_Stn where ObjTypeId=89 and Name <> 'HKCGB1' and Name <> 'TRN1' and Name <> 'SZLNG') 
+            AND TIME='%s %s:00:00' """ % (yesterday, i)
+        cursor.execute(sql)
+        row = cursor.fetchone()
+        row_value = row[0]
+        content1.append(row_value)
     # content = content.encode("utf-8")
-    return content
+    return [content1, content2]
     cursor.close()
     connection.close()
 
 
-def createPlot(data):
+def createPlot(data_today, data_yesterday):
     int_hour = int(hour)
     x_hour = range(0, int_hour)
-    x = np.array(x_hour)
-    y = np.array(data)
+    x1 = np.array(x_hour)
+    y1 = np.array(data_today)
+    x2 = np.array(x_hour)
+    y2 = np.array(data_yesterday)
     plt.rcParams['font.sans-serif'] = ['SimHei']
     plt.rcParams['axes.unicode_minus'] = False
     x_major_locator = MultipleLocator(1)
@@ -85,13 +97,16 @@ def createPlot(data):
     ax.xaxis.set_major_locator(x_major_locator)
     ax.yaxis.set_major_locator(y_major_locator)
     plt.ylim(0, 1400)
-    for a, b in zip(x, y):
+    for a, b in zip(x1, y1):
+        plt.text(a, b, '%.0f' % b, ha='center', va='bottom', fontsize=10)
+    for a, b in zip(x2, y2):
         plt.text(a, b, '%.0f' % b, ha='center', va='bottom', fontsize=10)
     plt.title("今日提气量曲线图")
     plt.xlabel("x - 小时")
     plt.ylabel("y - 气量（吨）")
     plt.grid()
-    plt.plot(x, y)
+    plt.plot(x1, y1)
+    plt.plot(x2, y2)
     file_name = 'D:/wehook_pic/'+time.strftime("%Y-%m-%d%H%M%S", time.localtime())+'.jpg'
     plt.savefig(file_name)
     return file_name
@@ -106,12 +121,16 @@ def cvtbase64(f1_name):
 
 
 if __name__ == "__main__":
-    day = time.strftime("%Y-%m-%d")
+    today = datetime.date.today()
+    oneday = datetime.timedelta(days=1)
+    yesterday = today-oneday
     hour = time.strftime("%H", time.localtime())
     # hour = 14
     wehook_url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=c14ed7e1-98b1-4eca-a3d1-0f392620789c&debug=1"   # 这个是你要调用的机器人地址，在群里添加了机器人之后就会有这个地址了，每个机器人只有一个地址
     post_data = querySql()
-    f_name = createPlot(post_data)
+    data_today = post_data[0]
+    data_yesterday = post_data[1]
+    f_name = createPlot(data_today, data_yesterday)
     cvt_result = cvtbase64(f_name)
     jpg_base64 = cvt_result[0]
     jpg_md5 = cvt_result[1]
