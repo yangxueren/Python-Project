69f608a658811cbf9f2a66b741ee661f28dff39b
yangxueren <yangxueren2017@gmail.com>
2021-11-14 00:08:01 +0800

    实现了推送今日和昨日提气数据，及深圳LNG是否反输的数据图表

--- WehookNotification/wechatRobot.py
+++ WehookNotification/wechatRobot.py
@@ -24,22 +24,6 @@ def webookRequest(url, jpg_base64, jpg_md5):
         else:
             print("发送失败")
         return response.text
-        # outmsg = """{
-        #     "msgtype": "image",
-        #     "image": {
-        #         "base64": "%s",
-        #         "md5": "%s"
-        #     }
-        # }""" %(jpg_base64, jpg_md5)
-        # response = requests.post(url, data=outmsg)
-        # response = response.json()
-        # response_str = json.dumps(response)
-        # print(response_str)
-        # if 'errcode' not in response_str:
-        #     result = "发送成功"
-        #     return result
-        # else:
-        #     return 'Error'
 
 def querySql():
     config_dit = {
@@ -75,19 +59,25 @@ def querySql():
         cursor.execute(sql)
         row = cursor.fetchone()
         row_value = row[0]
-        content1.append(row_value)
+        content2.append(row_value)
     # content = content.encode("utf-8")
-    return [content1, content2]
+
+    sql = """select SUM(isnull(Energy,0))/54.55 from gmspdb04.dbo.S2_HourlyStationBalance where + StnId in (select Id from gmspdb04.dbo.S2_Stn where ObjTypeId=89 and Name = 'SZLNG') 
+            AND SUBSTRING(CONVERT(VARCHAR(20),TIME,25),1,10) = '%s' """ % today
+    cursor.execute(sql)
+    row = cursor.fetchone()
+    content3 = row
+
+    return [content1, content2, content3]
     cursor.close()
     connection.close()
 
 
-def createPlot(data_today, data_yesterday):
+def createPlot(data_today, data_yesterday, data_szlng):
     int_hour = int(hour)
     x_hour = range(0, int_hour)
-    x1 = np.array(x_hour)
+    x = np.array(x_hour)
     y1 = np.array(data_today)
-    x2 = np.array(x_hour)
     y2 = np.array(data_yesterday)
     plt.rcParams['font.sans-serif'] = ['SimHei']
     plt.rcParams['axes.unicode_minus'] = False
@@ -97,17 +87,23 @@ def createPlot(data_today, data_yesterday):
     ax.xaxis.set_major_locator(x_major_locator)
     ax.yaxis.set_major_locator(y_major_locator)
     plt.ylim(0, 1400)
-    for a, b in zip(x1, y1):
+    for a, b in zip(x, y1):
         plt.text(a, b, '%.0f' % b, ha='center', va='bottom', fontsize=10)
-    for a, b in zip(x2, y2):
-        plt.text(a, b, '%.0f' % b, ha='center', va='bottom', fontsize=10)
+    # for a, b in zip(x, y2):
+    #     plt.text(a, b, '%.0f' % b, ha='center', va='bottom', fontsize=10)
     plt.title("今日提气量曲线图")
     plt.xlabel("x - 小时")
     plt.ylabel("y - 气量（吨）")
-    plt.grid()
-    plt.plot(x1, y1)
-    plt.plot(x2, y2)
-    file_name = 'D:/wehook_pic/'+time.strftime("%Y-%m-%d%H%M%S", time.localtime())+'.jpg'
+    plt.grid(axis="y")
+    plt.bar(x, y1, color=['#c5e0b4'], label='今日提气量')
+    plt.plot(x, y2, marker='o', label='昨日提气量')
+    plt.legend()
+    data_szlng = round(list(data_szlng)[0])`
+    if data_szlng == 0:
+        plt.annotate(xy=[6, 1200], s='今日深圳LNG未反输')
+    else:
+        plt.annotate(xy=[6, 1200], s='今日深圳LNG反输量为：%s吨' % data_szlng)
+    file_name = 'D:/wehook_notification/wehook_pic/'+time.strftime("%Y-%m-%d%H%M%S", time.localtime())+'.jpg'
     plt.savefig(file_name)
     return file_name
 
@@ -125,12 +121,12 @@ if __name__ == "__main__":
     oneday = datetime.timedelta(days=1)
     yesterday = today-oneday
     hour = time.strftime("%H", time.localtime())
-    # hour = 14
     wehook_url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=c14ed7e1-98b1-4eca-a3d1-0f392620789c&debug=1"   # 这个是你要调用的机器人地址，在群里添加了机器人之后就会有这个地址了，每个机器人只有一个地址
     post_data = querySql()
     data_today = post_data[0]
     data_yesterday = post_data[1]
-    f_name = createPlot(data_today, data_yesterday)
+    data_szlng = post_data[2]
+    f_name = createPlot(data_today, data_yesterday, data_szlng)
     cvt_result = cvtbase64(f_name)
     jpg_base64 = cvt_result[0]
     jpg_md5 = cvt_result[1]
